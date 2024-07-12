e6c5263de3397c500fd56aa10e965da61a91da08
FairTee <stephenodjidja7@gmail.com>
2024-07-12 08:56:53 -0700

    Created the Homepage

--- quiz_app/quiz_project/quiz_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib import admin
-from .models import Quiz, Question, UserQuizResult
-
-admin.site.register(Quiz)
-admin.site.register(Question)
-admin.site.register(UserQuizResult)
